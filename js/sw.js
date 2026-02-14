32bf346ebbe265016183074edb6f8ec3ff7cbb76
EngAli24 <engalihood@gmail.com>
2026-02-14 13:20:41 +0200

    Add notificationclick event listener
    Added notificationclick event listener to close notifications.

--- js/sw.js
+++ js/sw.js
@@ -1,2 +1,6 @@
-self.addEventListener("install", ()=> self.skipWaiting());
-self.addEventListener("activate", ()=> self.clients.claim());
+self.addEventListener("install", () => self.skipWaiting());
+self.addEventListener("activate", () => self.clients.claim());
+
+self.addEventListener("notificationclick", e => {
+ e.notification.close();
+});
