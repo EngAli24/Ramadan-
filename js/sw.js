ffad4381f0031c2475f8da0ff20a3a62ad499434
EngAli24 <engalihood@gmail.com>
2026-02-14 18:25:56 +0200

    Update sw.js

--- js/sw.js
+++ js/sw.js
@@ -1,6 +1,29 @@
-self.addEventListener("install", () => self.skipWaiting());
-self.addEventListener("activate", () => self.clients.claim());
+const CACHE_NAME = "noor-app-v1";
 
-self.addEventListener("notificationclick", e => {
- e.notification.close();
+const FILES = [
+ "/Ramadan_/",
+ "/Ramadan_/index.html",
+
+ "/Ramadan_/css/style.css",
+
+ "/Ramadan-/js/script.js",
+ "/Ramadan-/js/wird.js",
+ "/Ramadan-/js/azkar.js",
+ "/Ramadan-/js/challenges.js",
+ "/Ramadan-/js/profile.js",
+ "/Ramadan-/js/ramadan.js",
+
+ "/Ramadan-/js/adhan.mp3"
+];
+
+self.addEventListener("install", e => {
+ e.waitUntil(
+  caches.open(CACHE_NAME).then(c => c.addAll(FILES))
+ );
+});
+
+self.addEventListener("fetch", e => {
+ e.respondWith(
+  caches.match(e.request).then(r => r || fetch(e.request))
+ );
 });
